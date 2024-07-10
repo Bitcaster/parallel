c9acdee23c1bfa9e0f4b39bece6b531d1dc6a6c6
ArneFfm <arnekellmann@googlemail.com>
2024-07-10 21:09:11 +0200

    BAC-1483: added fake error handler to supress exception and restored error_handler afterwards

--- lib/Context/Internal/ProcessHub.php
+++ lib/Context/Internal/ProcessHub.php
@@ -75,7 +75,7 @@ class ProcessHub
             $this->server,
             static function (string $watcher, $server) use (&$keys, &$acceptor): void {
                 // Error reporting suppressed since stream_socket_accept() emits E_WARNING on client accept failure.
-                while ($client = @\stream_socket_accept($server, 0)) {  // Timeout of 0 to be non-blocking.
+                while ((\set_error_handler(function(){})) && $client = @\stream_socket_accept($server, 0) && \restore_error_handler()) {  // Timeout of 0 to be non-blocking.
                     asyncCall(static function () use ($client, &$keys, &$acceptor) {
                         $channel = new ChannelledSocket($client, $client);
 
